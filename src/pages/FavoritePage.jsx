6114034825abda82062f2d2ce695605063001f7e
saied83 <saiedtechit@gmail.com>
2024-08-26 19:44:11 +0600

    design ui

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,5 +1,27 @@
+import RecipeCard from "../ components/RecipeCard";
+
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  const fav = false;
+  return (
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" alt="404 svg" className="h-3/4" />
+          </div>
+        )}
+        {fav && (
+          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FavoritePage;
