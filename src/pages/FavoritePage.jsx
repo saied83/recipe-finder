fe1411a6be3520188a788fbb076b3974005a55dd
saied83 <saiedtechit@gmail.com>
2024-08-26 21:55:36 +0600

    complete Recipe Finder app

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,24 +1,23 @@
 import RecipeCard from "../ components/RecipeCard";
+import { getRandomColor } from "../utils/getRandomColor";
 
 const FavoritePage = () => {
-  const fav = false;
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
     <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
       <div className="max-w-screen-lg mx-auto">
         <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
-        {!fav && (
+        {!favorites.length === 0 && (
           <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="/404.svg" alt="404 svg" className="h-3/4" />
           </div>
         )}
-        {fav && (
-          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-          </div>
-        )}
+
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {favorites.map((recipe, indx) => (
+            <RecipeCard key={indx} recipe={recipe} {...getRandomColor()} />
+          ))}
+        </div>
       </div>
     </div>
   );
